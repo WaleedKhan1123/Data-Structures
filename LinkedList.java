6ade5002de48b1b49998a3fe70211ab83b81b407
waleed-khan11 <waleed.kn270@gmail.com>
2024-10-28 00:19:18 +0500

    LinkedList

--- LinkedList.java
+++ LinkedList.java
@@ -3,7 +3,7 @@ public class LinkedList<T> {
 
         T data;
         Node next;
-
+        
     }
     Node Start;
 
@@ -35,11 +35,37 @@ public class LinkedList<T> {
             _t.next = n;
           }
     }
-    public void insertAfter(){
-        
+    public void insertAfter(T key,T Value){
+        if(Start==null){
+            System.out.println("List is Empty");
+        }
+        else{
+            Node _t =Start;
+            while(_t!=null){
+                     if(_t.data==key){
+                        Node n = new Node();
+                        n.data = Value;
+                        n.next= _t.next;
+                        _t.next=n;
+                        return;
+                     }
+                _t = _t.next;
+
+            }
+            System.out.println("Key not Found");
+        }
     }
     public T deletAtStart(){
-        return null;
+        if(Start==null){
+           System.out.println("List is empty");
+           return null;
+        }
+        else{
+           T data = Start.data;
+           Start= Start.next;
+           return data;
+        }
+        
     }
     public T deletAtEnd(){
         return null;
@@ -77,6 +103,12 @@ public class LinkedList<T> {
         list.print();
         list.insertAtEnd(6);
         list.insertAtEnd(102);
+        list.insertAfter(102, 3);
         list.print();
+        list.insertAfter(6, 7);
+        list.deletAtStart();
+        list.deletAtStart();
+        list.print();
+
     }
 }
